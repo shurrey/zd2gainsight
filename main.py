0637c3277a7db0601c4a7e765ef8f3a9b2b9cdae
Scott Hurrey <scott.hurrey@gmail.com>
2024-03-16 16:23:08 -0400

    update authentication and token management

--- main.py
+++ main.py
@@ -24,11 +24,10 @@ auth = auth.AuthController()
 
 
 def create_post(post):
-    print(f"CREATE POST")
+    
     url = f"{gdh_domain}/v2/conversations/start?authorId=646&moderatorId=646"
-    print(f"CREATE POST url {url}")
     token = auth.get_token()
-    print(f"CREATE POST get_token {token}")
+    
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {token}"
@@ -76,26 +75,25 @@ def reply_to_conversation(comment, conversation_id):
         return None
 
 def get_posts(url):
-    print(f"GET POSTS")
+    
     response = requests.request(
         "GET",
         url,
         headers=zd_headers
     )
-    #print(f"GET POSTS response {response.text}")
+    
     post_json = response.json()
-    #print(f"GET POSTS get_json")
     posts = post_json['posts']
-    #print(f"GET POSTS posts {posts}")
+    
     for post in posts:
-        #print(f"GET POSTS post {post}")
+    
         conversation_id = create_post(post)
 
         if conversation_id != "UNKNOWN":
             if post['comment_count'] > 0:
                 get_comments(post['id'], conversation_id)
         else:
-            print(f"Error processing")#{post}
+            print(f"GET POSTS: Error creating post in Gainsight")
         
     return(post_json['next_page'])
 
@@ -123,11 +121,10 @@ url = zd_url
 
 while more:
     try:
-        print(f"Calling get_posts {i}")
         new_url = get_posts(url)
 
         url = new_url
-        i+=1
+        
     except Exception as e:
         print (f"no more {e}")
         more = False
